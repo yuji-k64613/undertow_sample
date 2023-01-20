2f0045f18eca391a001d3c419a5294d8022b0d30
Yuji Konishi <yuji.k64613@gmail.com>
2023-01-20 18:18:37 +0900

    URL

--- lib/src/main/java/com/demo/DBUtil.java
+++ lib/src/main/java/com/demo/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
 	private JedisPool pool;
 
 	private DBUtil() {
-		pool = new JedisPool("redis://red-cf55jb5a4992g5g872t0", 6379);
+		pool = new JedisPool("redis://red-cf55jb5a4992g5g872t0:6379");
 		try (Jedis jedis = pool.getResource()) {
 			jedis.set("hello", "Redis");
 			jedis.set("key", "value");
