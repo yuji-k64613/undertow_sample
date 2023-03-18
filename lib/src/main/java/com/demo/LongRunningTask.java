6b09b376354cb6d5252325fa668193a9ce640d93
Yuji Konishi <yuji.k64613@gmail.com>
2023-03-19 06:02:49 +0900

    updated

--- lib/src/main/java/com/demo/LongRunningTask.java
+++ lib/src/main/java/com/demo/LongRunningTask.java
@@ -7,6 +7,13 @@ import org.springframework.web.client.RestTemplate;
 public class LongRunningTask implements Runnable {
 	final static Logger logger = LoggerFactory.getLogger(LongRunningTask.class);
 	private static int n = 0;
+	private String url1 = "";
+	private String url2 = "";
+	
+	public LongRunningTask() {
+        url1 = System.getenv("URL1");
+        url2 = System.getenv("URL2");		
+	}
 	
 	@Override
 	public void run() {
@@ -18,8 +25,7 @@ public class LongRunningTask implements Runnable {
         // Define the URL you want to send the request to
         //String url = "https://undertow-sample.onrender.com/?key=hello";
         //String url = "https://undertow-sample-scheduler.onrender.com/?key=hello";
-        String key = (n == 0)? "URL1" : "URL2";
-        String url = System.getenv(key);
+        String url = (n == 0)? url1 : url2;
 
         // Send the HTTP GET request and receive the response as a string
         String response = restTemplate.getForObject(url, String.class);
